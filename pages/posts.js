8f1db9038671000d2b4a61d3814c514daa1b15c7
Anthony Tancredi <anthony197542@gmail.com>
2026-08-04 20:33:31 +0000

    fix routes and navbar

--- pages/posts.js
+++ pages/posts.js
@@ -1,17 +1,5 @@
-import Link from "next/link"
-import styles from "../styles/Navbar.module.css"
-
-export default function Navbar() {
-  return (
-    <nav className={styles.nav}>
-      <ul className={styles.ul}>
-        <li><Link href="/">Home</Link></li>
-        <li><Link href="/posts">Posts</Link></li>
-        <li><Link href="/users">Users</Link></li>
-        <li><input type="text" /></li>
-      </ul>
-    </nav>
-  )
+export default function Posts() {
+  return <h1>This is the Posts page</h1>;
 }
 
 
