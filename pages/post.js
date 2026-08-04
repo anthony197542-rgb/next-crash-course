b90ced3fa483d62bee836a04366376e2e7beb952
Anthony Tancredi <anthony197542@gmail.com>
2026-08-04 12:49:57 +0000

    next-crash-course

--- pages/post.js
+++ pages/post.js
@@ -1,5 +1,12 @@
-export default function Post() {
-    return (
-        <h1>This is the Post page</h1>
-    )
+import Link from "next/link";
+
+export default function Post({ post }) {
+  return (
+    <div className="card">
+      <Link href={`/posts/${post.id}`}>
+        <h3 style={{ textTransform: "capitalize" }}>{post.title}</h3>
+      </Link>
+      <p style={{ marginTop: "8px", color: "#666" }}>{post.body}</p>
+    </div>
+  );
 }
