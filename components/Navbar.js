2bfdcebabf6c18324b4d5f735bfd55c41c0a95b9
Anthony Tancredi <anthony197542@gmail.com>
2026-08-04 12:28:07 +0000

    next-crash-course

---
+++ components/Navbar.js
@@ -0,0 +1,17 @@
+import styles from "../styles/Navbar.module.css";
+import Link from "next/link";
+
+
+
+export default function Navbar() {
+ 
+  return (
+    <nav className={styles.nav}>
+      <ul className={styles.ul}>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/posts">Posts</Link></li>
+        <li><Link href="/users">Users</Link></li>
+        </ul>
+    </nav>
+  )
+}
---
+++ pages/post.js
@@ -0,0 +1,5 @@
+export default function Post() {
+    return (
+        <h1>This is the Post page</h1>
+    )
+}
